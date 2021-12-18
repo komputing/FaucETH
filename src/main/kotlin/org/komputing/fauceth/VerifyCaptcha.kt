be4d3c49a358f8016bf93023fb553a11d926d6d9
ligi <ligi@ligi.de>
2021-12-18 17:10:15 +0100

    Cleanup verifyCaptcha

--- src/main/kotlin/org/komputing/fauceth/VerifyCaptcha.kt
+++ src/main/kotlin/org/komputing/fauceth/VerifyCaptcha.kt
@@ -7,24 +7,18 @@ import java.net.http.HttpResponse
 import java.time.Duration
 
 // very crude for now
-fun verifyCaptcha(response: String, secret: String): Boolean {
-    var sb = StringBuilder()
-    sb.append("response=")
-    sb.append(response)
-    sb.append("&secret=")
-    sb.append(secret);
+fun verifyCaptcha(hCaptchaResponse: String, hCaptchaSecret: String): Boolean {
+    val body = "response=$hCaptchaResponse&secret=hCaptchaSecret"
 
-    val httpClient = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(5))
-        .build();
+    val httpClient = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(5)).build();
 
     val request = HttpRequest.newBuilder()
         .uri(URI.create("https://hcaptcha.com/siteverify"))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .timeout(Duration.ofSeconds(10))
-        .POST(HttpRequest.BodyPublishers.ofString(sb.toString())).build();
+        .POST(HttpRequest.BodyPublishers.ofString(body.toString())).build();
 
     val response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-    System.out.println(response.body())
     return response.statusCode() == 200 && response.body().contains("\"success\":true")
 }
