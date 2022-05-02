5d709fab7d3c2dee8b4f7e4907d951d30b9d0c8e
ligi <ligi@ligi.de>
2022-05-02 18:50:25 +0200

    Migrate to KTor 2.0.1

--- src/main/kotlin/org/komputing/fauceth/calls/IndexCall.kt
+++ src/main/kotlin/org/komputing/fauceth/calls/IndexCall.kt
@@ -1,8 +1,8 @@
 package org.komputing.fauceth.calls
 
-import io.ktor.application.*
-import io.ktor.html.*
-import io.ktor.response.*
+import io.ktor.server.application.*
+import io.ktor.server.html.*
+import io.ktor.server.response.*
 import io.ktor.util.pipeline.*
 import kotlinx.html.*
 import org.kethereum.ens.isPotentialENSDomain
